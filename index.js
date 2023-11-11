0326bf5778c9e56348fbd602eb26d29537c117da
gokuls1157 <gokuls1157@gmail.com>
2023-11-11 23:12:34 +0530

    new commit

--- index.js
+++ index.js
@@ -18,8 +18,24 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:3000",
+    optionsSuccessStatus:200,
   })
 );
+
+app.use((req,res,next)=>{
+   res.setHeader("Access-Control-Allow-Origin","*");
+   res.setHeader("Access-Control-Request-Method","*");
+   res.setHeader("Access-Control-Allow-Methods","GET,PUT,POST,DELETE,PATCH");
+   res.setHeader("Access-Control-Allow-Headers","Content-Type");
+   res.setHeader("Access-Control-Expose-Headers","Content-Type");
+
+   if(req.method==="OPTIONS"){
+      res.writeHead(200);
+      return res.end();
+   }else{
+      return next();
+   }
+})
 app.use("/students",StudentsRouter);
 
  app.get('/',function(req,res){
