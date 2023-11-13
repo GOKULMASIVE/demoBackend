93059abf9ea0f3911c6a23655f91d9dadb85dd2e
gokuls1157 <gokuls1157@gmail.com>
2023-11-13 10:13:50 +0530

    new one

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ export const client=new MongoClient(MONGO_URL);
 await client.connect();
 console.log("Mongo is connected");
 
-app.use(express.json());
-app.use(
-  cors()
-);
-
+app.use(cors());
+app.use(express.json({limit:"25mb"}))
+app.use(express.urlencoded({limit:"25mb",extended:true}))
+app.use((req,res,next)=>{
+   res.setHeader("Access-Control-Allow-Origin","*")
+   next();
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended:false
