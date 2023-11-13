91b1a169558ad1f2dfafb722589635158a286605
gokuls1157 <gokuls1157@gmail.com>
2023-11-13 10:25:26 +0530

    new

--- routes/student.route.js
+++ routes/student.route.js
@@ -17,11 +17,11 @@ router.get("/", async function (req, res) {
   res.send(result);
 });
 
-router.post("/", async function (req, res) {
-  // const { id } = req.params;
-  // const objId = new ObjectId(id);
-  // const result = await getStudentById(objId);
-  const data=req.body
+router.post("/:id", async function (req, res) {
+  const { id } = req.params;
+  const objId = new ObjectId(id);
+  const result = await getStudentById(objId);
+  // const data=req.body
   // console.log(result);
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   const email= await sendMail(data,req,res);
